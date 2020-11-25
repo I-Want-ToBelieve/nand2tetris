7ff9161068b324bef2684ce3d2e0b1d434de8c6e
doublethinkio <floatshuyin@gmail.com>
2020-11-26 04:30:02 +0800

    fill.asm and mult.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,116 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@24576 // 256 * 32 + 16384 = 24576
+D=A
+@count
+M=D
+
+@color // black = -1  white = 0
+M=0
+
+@successively // increasing = 1 descending = -1
+M=1
+
+@SCREEN // base
+D=A
+@current
+M=D
+
+
+(EndlessLoop)
+// if key = 0 goto DrawWhite
+@KBD
+D=M
+@DrawWhite
+D;JEQ
+// else goto DrawBlack
+@DrawBlack
+0;JMP
+
+
+(DrawBlack)
+// if color !== white goto EndlessLoop
+@color
+D=M+1 // -1 + 1
+@EndlessLoop
+D;JEQ
+// color = black  successively = 1
+@color
+M=-1
+@successively
+M=1
+// else goto Draw
+@Draw
+0;JMP
+
+(DrawWhite)
+// if color !== black goto EndlessLoop
+@color
+D=M
+@EndlessLoop
+D;JEQ
+// color = white  successively = -1
+@color
+M=0
+@successively
+M=-1
+// else goto Draw
+@Draw
+0;JMP
+
+(Draw)
+@color
+D=M
+@current
+A=M
+M=D //  draw current 16 bit (white or black)
+
+@successively
+D=M
+@current // current = current + 1 or current - 1
+M=M+D
+
+// if color = 0 (white) goto White else goto Black
+@color
+D=M
+@White
+D;JEQ
+@Black
+0;JMP
+
+(White)
+// if current == @SCREEN - 1 goto EndlessLoop
+@SCREEN
+D=A
+@current
+D=D-M
+// goto EndlessLoop
+@EndlessLoop
+D-1;JEQ
+// else goto Draw
+@Draw
+0;JMP
+
+(Black)
+// if current == count goto EndlessLoop
+@count
+D=M
+@current
+D=D-M
+// goto EndlessLoop
+@EndlessLoop
+D;JEQ
+// else goto Draw
+@Draw
+0;JMP
+
+
+
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,77 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// a: args = meb[0]
+@R0 // A = 0
+D=M // D = Meb[A] = Meb[0]
+@a
+M=D
+
+// b: args = meb[1]
+@R1 // A = 1
+D=M // D = Meb[A] = Meb[1]
+@b
+M=D
+
+// result = 0
+@R2 // A = 2
+M = 0 // Meb[A] = Meb[2] = 0
+
+// if a === 0 || b === 0 then goto end
+@a
+D=M
+@end
+D;JEQ
+@b
+D=M
+@end
+D;JEQ
+// else goto mult
+@mult
+0;JMP
+
+
+(mult)
+@a
+D=M
+@R2 // A = 2
+// result = result + a
+M = M + D
+
+// if b < 0 goto increment
+@b
+D=M
+@increment
+D;JLT
+// else if b > 0 goto decrease
+@decrease
+D;JGT
+
+
+(increment)
+@b
+M = M + 1
+D=M
+// if b === 0 goto end
+@end
+D;JEQ
+// else goto mult
+@mult
+0;JMP
+
+(decrease)
+@b
+M = M - 1
+D=M
+// if b === 0 goto end
+@end
+D;JEQ
+// else goto mult
+@mult
+0;JMP
+
+
+(end)
+@end
+0;JMP
